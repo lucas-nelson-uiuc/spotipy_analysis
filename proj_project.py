76e14b5d358fcc71f699c86984f4be98d9c87bfd
Lucas Nelson <lucas.nelson.uiuc@gmail.com>
2021-07-17 15:12:14 -0500

    Update proj_project.py

--- proj_project.py
+++ proj_project.py
@@ -29,28 +29,9 @@ st.title('Spotify Analysis Dashboard')
 
 # SIDEBAR page layout
 with st.sidebar.beta_expander('Enter Spotify Details', False):
-    client_id = st.text_input('Client ID', '3369a177760443e1ba8fdc24ffe8ee3a')
-    client_secret = st.text_input('Client secret', '26557253d83447879b0ff7251d291517')
-    playlist_input = st.text_area('Playlist URL(s)', """https://open.spotify.com/playlist/2peFCkryOU68kcEueeBmcw,
-        https://open.spotify.com/playlist/4gRAQPeK0VBqua9EVCk83i,
-        https://open.spotify.com/playlist/24fobBkjvpmwUL6M55Ls41,
-        https://open.spotify.com/playlist/7fzOIIrfWEifFp64mZr0Fp,
-        https://open.spotify.com/playlist/4ruz6qz9UaJi0Uh9aXWd4e,
-        https://open.spotify.com/playlist/1EffEt6r2PZiNoqJPBa53S,
-        https://open.spotify.com/playlist/3PFpKt44V2PP5IvNqCn1ly,
-        https://open.spotify.com/playlist/2dijCoBx6ktdHC7OjERJHD,
-        https://open.spotify.com/playlist/4r3lbgLtB6OflmHdNAeFWt,
-        https://open.spotify.com/playlist/55mC6DTHx1jWpHUfXpUaUC,
-        https://open.spotify.com/playlist/4I9peD1SiBDaBhKsDNa4yg,
-        https://open.spotify.com/playlist/3Whz31feyEWBBJ1bgubprI,
-        https://open.spotify.com/playlist/0QPTp6QO7mt3icX7NiFax6,
-        https://open.spotify.com/playlist/57Q4NLC64QOuJcqzzvAioi,
-        https://open.spotify.com/playlist/2CcSamqgDw8BzN0RJp7qGA,
-        https://open.spotify.com/playlist/45ZeJcyQ9oEIf4Eo9aJ4Bt,
-        https://open.spotify.com/playlist/2JUdrxncd30zv3VRJkLaZS,
-        https://open.spotify.com/playlist/5mNmEqtjAnqjXaVFkNZ5ET,
-        https://open.spotify.com/playlist/3DInsqW7PC1gDisXkIV22x
-    """)
+    client_id = st.text_input('Client ID', '') # enter client_id
+    client_secret = st.text_input('Client secret', '') # enter client_secret
+    playlist_input = st.text_area('Playlist URL(s)', '') # enter playlist url(s)
     ready_button = st.checkbox('Gather Data Frame')
 
 with st.sidebar.beta_expander('Dashboard Resources', False):
@@ -163,15 +144,7 @@ with st.beta_container():
     hist_hist = hist_row1[1].pyplot(plot_histogram(f_data))
     
 
-def plot_nested_pies(df):
-    return st.pyplot(proj_plot.proj_plot_nestpie(df, 'genre'))
+# def plot_nested_pies(df):
+#     return st.pyplot(proj_plot.proj_plot_nestpie(df, 'genre'))
 
-plot_nested_pies(f_data)
-
-
-
-# if st.button('Search'):
-#    proj_analysis.update_search()
-
-# if st.button('Filter'):
-#    proj_analysis.update_filter()
+# plot_nested_pies(f_data)
