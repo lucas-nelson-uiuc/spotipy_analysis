a704bcc9eaa5d3b8de0245b07de95d001ca9031d
Lucas Nelson <lucas.nelson.uiuc@gmail.com>
2021-09-19 15:24:49 -0500

    update non-local paths

--- scripts/proj_project.py
+++ scripts/proj_project.py
@@ -227,7 +227,7 @@ def project_welcm_page():
     data_grp[0].markdown('''
         Data for your dashboard is gathered by providing playlist URLs
 
-        Visit the [`Walkthrough Document`](https://github.com/lucas-nelson-uiuc/academia_epidemia/blob/main/spotipy_analysis/WALKTHROUGH_DOCUMENT.md#Interacting-with-Plotly-Graphs)
+        Visit the [`Walkthrough Document`](https://github.com/lucas-nelson-uiuc/spotipy_analysis/blob/main/docs/walk_through.md)
         to learn how to properly prepare your input data
     ''')
 
